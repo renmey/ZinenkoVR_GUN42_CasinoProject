db9e3e2b100c69e623883272f7ec5ee3303f699e
renmey <deadbeat686@gmail.com>
2026-04-20 22:54:25 +0300

    Добавьте файлы проекта.

---
+++ CasinoProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CasinoProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
